20631f5657cf073fa10be10447d920c5b3aed0c4
ARMET6 <adrian.razo.mandujano.11@gmail.com>
2026-02-16 15:18:59 -0600

    Mejora de manejo de errores en DB y limpieza de rutas

--- src/models/User.js
+++ src/models/User.js
@@ -18,4 +18,4 @@ userSchema.methods.comparePassword = async function(enteredPassword) {
     return await bcrypt.compare(enteredPassword, this.password);
 };
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('users', userSchema);
